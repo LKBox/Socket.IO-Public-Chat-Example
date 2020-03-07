fa3386b8137a6cb8d713e8b5ea469acd3b76f5ec
Levent Karadağ <leventkaradag@Levents-iMac.local>
2020-03-07 16:05:44 +0300

    message storage

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var stMessages = [];
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function (socket) {
-    socket.on('chat message', function (msg) {
+io.on('connection', function(socket) {
+    socket.on('firstconn', function(username) {
+        io.emit('firstconn', stMessages, username);
+    });
+    socket.on('chat message', function(msg) {
+        stMessages.push(msg);
+        if (stMessages.length >= 200) {
+            stMessages.shift();
+        }
         io.emit('chat message', msg);
     });
 });
 
-http.listen(process.env.PORT || 3000, function () {
+http.listen(process.env.PORT || 3000, function() {
     console.log('listening on *:' + process.env.PORT);
 });
